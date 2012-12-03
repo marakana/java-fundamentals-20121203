8cf5625f9a5b94e4631de2fa35a8bae3ffbec129
Dan Rosen <dan@marakana.com>
2012-12-03 13:03:04 -0600

    added Hello project and video 1.2

--- README.md
+++ README.md
@@ -3,3 +3,4 @@ java-fundamentals-20121203
 
 Videos:
   * https://mrkn.s3.amazonaws.com/recordings/java-fundamentals-cisco-20121203/1.1.mov
+  * https://mrkn.s3.amazonaws.com/recordings/java-fundamentals-cisco-20121203/1.2.mov
---
+++ hello/src/Hello.java
@@ -0,0 +1,22 @@
+/**
+ * Hello is a class that greets the world.
+ * 
+ * @author dr
+ * @since 1.0
+ */
+public class Hello {
+
+	/**
+	 * The main method is the special method that the JVM calls when the program
+	 * is run.
+	 * 
+	 * @param args
+	 *            the list of command-line arguments that you ran the program
+	 *            with
+	 */
+	public static void main(String[] args) {
+		String hello = "Hello, World";
+		System.out.println(hello);
+	}
+
+}
