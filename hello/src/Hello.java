97789e3becea2dfd3f525693442d5a9efd9d4414
Dan Rosen <dan@marakana.com>
2012-12-03 15:29:10 -0600

    hello with exception-handling

--- hello/src/Hello.java
+++ hello/src/Hello.java
@@ -15,8 +15,11 @@ public class Hello {
 	 *            with
 	 */
 	public static void main(String[] args) {
-		String name = args[0];
-		System.out.println("Hello, " + name);
+		try {
+			String name = args[0];
+			System.out.println("Hello, " + name);
+		} catch (ArrayIndexOutOfBoundsException e) {
+			System.out.println("Hello, Nobody.");
+		}
 	}
-
 }
