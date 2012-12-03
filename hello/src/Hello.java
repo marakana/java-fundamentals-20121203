1ab0ed37054ac067175792e7f34bffab37cd404e
Dan Rosen <dan@marakana.com>
2012-12-03 16:30:18 -0600

    demo: switch statements

--- hello/src/Hello.java
+++ hello/src/Hello.java
@@ -23,4 +23,31 @@ public class Hello {
 		}
 	}
 
+	// example of case statements with intentionally missing breaks
+	public static void printDaysInMonth(int num) {
+		int days;
+		switch (num) {
+		case 2:
+			days = 28;
+			break;
+		case 4:
+		case 6:
+		case 9:
+		case 11:
+			days = 30;
+			break;
+		case 1:
+		case 3:
+		case 5:
+		case 7:
+		case 8:
+		case 10:
+		case 12:
+			days = 31;
+			break;
+		default:
+			throw new IllegalArgumentException();
+		}
+		System.out.println(days);
+	}
 }
