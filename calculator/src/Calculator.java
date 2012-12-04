aa698b42f11a5977b5bf04004feafd3e3f218878
Dan Rosen <dan@marakana.com>
2012-12-04 10:49:12 -0600

    calculator step 2: split expression into tokens

--- calculator/src/Calculator.java
+++ calculator/src/Calculator.java
@@ -5,10 +5,13 @@ public class Calculator {
 		if (args.length != 1) {
 			System.err.println("Usage: Calculator <expression>");
 		} else {
+			// the expression is the first command-line argument
 			String expression = args[0];
-			
-			// step 2: split the string into "tokens," using spaces as delimiters
 
+			// split the expression into tokens, using space as the delimiter
+			String[] tokens = expression.split(" ");
+
+			
 			// step 3: loop through all of the tokens
 			//   if you see a number: push it on the stack
 			//   if you see an operator:
