286dc04ec5ba8c29dc8f1f3ed7282b4391bf8227
Dan Rosen <dan@marakana.com>
2012-12-07 10:38:13 -0600

    Square extends Rectangle

--- shapes/src/Square.java
+++ shapes/src/Square.java
@@ -1,47 +1,7 @@
-public class Square {
-
-	private int side;
+public class Square extends Rectangle {
 
 	public Square(int side) {
-		this.side = side;
-	}
-
-	public int getSide() {
-		return side;
-	}
-
-	public void setSide(int side) {
-		this.side = side;
-	}
-	
-	public int getArea() {
-		return side * side;
-	}
-
-	public int getPerimeter() {
-		return 4 * side;
-	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + side;
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Square other = (Square) obj;
-		if (side != other.side)
-			return false;
-		return true;
+		super(side, side);
 	}
 
 }
